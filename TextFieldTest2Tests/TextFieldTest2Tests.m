34d0d15b2ea1442789cafabf1d454f0dffb115bc
SeungyoupBaek <bsy8220@naver.com>
2014-01-03 17:53:03 +0900

    Initial Commit

---
+++ TextFieldTest2Tests/TextFieldTest2Tests.m
@@ -0,0 +1,34 @@
+//
+//  TextFieldTest2Tests.m
+//  TextFieldTest2Tests
+//
+//  Created by SDT-1 on 2014. 1. 3..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TextFieldTest2Tests : XCTestCase
+
+@end
+
+@implementation TextFieldTest2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
